c75044d10513561a2d9303e54f213c13e0915b11
qolors <cmickyg@gmail.com>
2022-07-21 22:19:47 -0400

    Almost have match logic finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import shuffle from './utilities/shuffle';
 import './App.css';
 import Card from './components/Card';
@@ -6,26 +6,88 @@ import Card from './components/Card';
 function App() {
 
   const [cards, setCards] = useState(shuffle);
+  const [pickOne, setPickOne] = useState(null);
+  const [pickTwo, setPickTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const [wins, setWins] = useState(0);
+
+  const handleClick = (card) => {
+    if (!disabled) {
+      pickOne ? setPickTwo(card) : setPickOne(card);
+    }
+  }
+
+  const handleTurn = () => {
+    setPickOne(null);
+    setPickTwo(null);
+    setDisabled(false);
+  }
+
+  useEffect(() => {
+    let pickTime;
+
+    if (pickOne && pickTwo) {
+
+      if (pickOne.image === pickTwo.image) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.image === pickOne.image) {
+              return { ...card, matched:true };
+            } else {
+              return card;
+            }
+          });
+        });
+        handleTurn();
+      } else {
+
+        setDisabled(true);
+
+        pickTime = setTimeout(() => {
+          handleTurn();
+        }, 1000);
+      
+      }
+
+    }
+
+    return () => {
+        clearTimeout(pickTime);
+      };
+  }, [cards, pickOne, pickTwo]);
+
+  useEffect(() => {
+
+    const checkWin = cards.filter((card) => !card.matched);
+
+    if (cards.length && checkWin.length < 1) {
+      console.log('You Win!');
+      setWins(wins + 1);
+      handleTurn();
+      setCards(shuffle);
+    }
+  }, [cards, wins]);
+
+
 
 
   return (
-    <div className='flex flex-row w-full h-screen'>
-    <div className="grid grid-cols-4 mx-auto px-2 gap-4 h-1/2">
+
+    <div className="grid grid-cols-4 h-screen mx-auto p-16 gap-4">
       { cards.map((card) => {
-        const { image, id, matched } = card
+        const { image, id, matched } = card;
 
         return (
           <Card
             key={id}
             image={image}
             selected={false}
-            onClock={() => {}}
+            onClick={() => handleClick(card)}
           />
         )
 
       })}
 
-    </div>
     </div>
   );
 }
--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,9 +1,10 @@
 const Card = ({ image, selected, onClick}) => {
 
     return (
-        <div className="w-full h-96 rounded-lg border border-red-100 bg-red-500 flex flex-row justify-center place-items-center">
-            <div className={selected ? 'selected' : 'not-selected'}>
-                <img alt='' src="/champs/Shaco.png" />
+        <div className=" transition duration-150 ease-in-out w-full rounded-lg border border-red-100 bg-red-500 flex flex-row justify-center place-items-center">
+            <div className={selected ? 'selected' : ''}>
+                <img alt='' src={image} className={selected ? 'opacity-100' : 'opacity-0'} />
+                <img alt='' onClick={onClick} src="/champs/Shaco.png" className={selected ? 'opacity-0' : 'opacity-100'} />
             </div>
         </div>
     )
